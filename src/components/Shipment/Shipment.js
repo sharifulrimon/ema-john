e9cae94eb0ed43676ef8d9f8ed878dc75fa6fa82
sharifulrimon <40694130+sharifulrimon@users.noreply.github.com>
2021-08-03 12:00:20 +0600

    first commit

---
+++ src/components/Shipment/Shipment.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import './Shipment.css';
+import { useContext } from 'react';
+import { UserContext } from '../../App';
+
+const Shipment = () => {
+
+
+  return (
+   <h1>Hello</h1>
+  );
+};
+
+export default Shipment;
